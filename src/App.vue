6e82a2205676f0b1003782abd8291b33fac21ac3
Munirabonu <muniratillaxojayeva@gmail.com>
2023-12-19 12:32:30 +0500

    add: style

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script lang="ts">
-interface ListItem {
-  id: number;
-  text: string;
-}
+import VButton from '../src/generic/VButton.vue'
+import Filter from './component/Filter.vue'
+import { defineComponent, PropType } from 'vue';
 
-export default {
+export default defineComponent({
+  components: {
+    Filter,
+    VButton
+  },
+
   data() {
     return {
       item: '' as string,
@@ -41,10 +45,10 @@ export default {
       this.editState = false
     },
     sort() {
-      this.listItem.sort((a:object, b:object) => a.text.localeCompare(b.text))
+      this.listItem.sort((a: object, b: object) => a.text.localeCompare(b.text))
     }
   },
-}
+})
 
 
 </script>
@@ -55,54 +59,51 @@ export default {
     <h3 class="">Enter text into the input field to add items to your list. </h3>
     <h3>Click the item to mark it as complete. </h3>
     <h3>Click the "X" to remove the item from your list. </h3>
-
     <div class="add">
       <input type="text" placeholder="New item...." v-model="item" @keydown.enter="addItem">
-      <button @click="addItem">add</button>
-      <button @click="sort">alphabetically</button>
+      <VButton color="primary" @click="addItem">add</VButton>
+      <VButton @click="sort" color="success">alphabetically</VButton>
     </div>
-    <table>
-      <div v-for="(list, index) in listItem" :key="index">
+    <div class="wrap">
+      <div v-for="(list, index) in listItem" :key="index" class="list">
+        <div v-if="editState && edit.id === list.id" class="item">
+          <input type="text" v-model="edit.text" @keydown.enter="saveItem">
+          <VButton @click="saveItem(list)" color="info">save</VButton>
+        </div>
+        <div v-else class="item">
 
-        <tr v-if="editState && edit.id === list.id">
-          <td><input type="text" v-model="edit.text" @keydown.enter="saveItem"></td>
-          <td><button @click="saveItem(list)">save</button></td>
-        </tr>
-        <tr v-else>
-          <td>{{ index + 1 }}</td>
-          <td>{{ list.text }}</td>
-          <td><button @click="deleteItem(list.id)">delete</button></td>
-          <td><button @click="editItem(list)">edit</button></td>
-        </tr>
+          <p>{{ index + 1 }} {{ list.text }}</p>
+          <VButton @click="deleteItem(list.id)" color="danger" bold>x</VButton>
+          <VButton @click="editItem(list)" color="warning">edit</VButton>
+        </div>
       </div>
-    </table>
+    </div>
 
   </div>
 </template>
-<style>
+<style scoped>
 .bg-blue {
   width: 100%;
   height: 100vh;
   max-width: 100vw;
   min-height: 100vh;
   background-color: #04A1BF;
-  text-align: center;
   color: white;
   display: flex;
   flex-direction: column;
   align-items: center;
-  
 }
 
 .add {
   display: flex;
-  height: 30px;
+  align-items: center;
+  gap: 10px;
 }
 
-.bg-blue .add input {
+ input {
   min-width: 65%;
   width: 500px;
-  padding: 5px;
+  padding: 10px;
   border: none;
   outline: none;
   border-radius: 5px;
@@ -113,13 +114,33 @@ input::placeholder {
   font-size: 18px;
 }
 
-button {
-  border: none;
-  padding: 5px 15px;
-  border-radius: 5px;
-  color: #04A1BF;
-  background-color: #025F70;
-  transition: all 0.75s ease;
-  -webkit-transition: all 0.75s ease;
+.wrap {
+  width: 50%;
+  min-width: 600px;
+  max-width: 1000px;
+  margin: 20px auto;
+  display: grid;
+  justify-items: center;
+  gap: 25px;
+  max-height: 60vh;
+  overflow: auto;
+
 }
+
+.list {
+  width: 100%;
+  background-color: #06758b;
+  border-radius: 5px;
+  transition: all 1s linear;
+}
+
+.item {
+  padding: 10px;
+  display: grid;
+  grid-template-columns: 1fr 10% 10%;
+  gap: 10px;
+  justify-content: space-between;
+  align-items: center;
+}
+
 </style>
